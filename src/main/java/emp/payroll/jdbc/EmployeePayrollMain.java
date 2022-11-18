e513d248a2c54e0b6086f93203def6c7fe8a76fb
singh36Pushpendra <psthakur.1999@gmail.com>
2022-11-18 16:13:18 +0530

    Added: UC1 Connection established to MySQL DB

--- src/main/java/emp/payroll/jdbc/EmployeePayrollMain.java
+++ src/main/java/emp/payroll/jdbc/EmployeePayrollMain.java
@@ -1,6 +1,17 @@
+package emp.payroll.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class EmployeePayrollMain {
     public static void main(String[] args) {
         System.out.println("Welcome to JDBC for Employee Payroll!");
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_service", "root", "root")) {
+            System.out.println("Connection established successfully!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
