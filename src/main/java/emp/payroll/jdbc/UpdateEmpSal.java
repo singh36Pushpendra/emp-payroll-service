3829496bc236a8347e4fda3969607c66e4fa61de
singh36Pushpendra <psthakur.1999@gmail.com>
2022-11-20 16:56:16 +0530

    Added: UC4 Updated salary using PreparedStatement

--- src/main/java/emp/payroll/jdbc/UpdateEmpSal.java
+++ src/main/java/emp/payroll/jdbc/UpdateEmpSal.java
@@ -1,21 +1,22 @@
 package emp.payroll.jdbc;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class UpdateEmpSal {
 
     private static final String QUERY = "UPDATE employee_payroll SET salary = 3e6 WHERE name = 'Terissa';";
 
     static double getUpdatedSalary() throws SQLException {
+        Connection con = UseJdbc.useConnection();
+        PreparedStatement prepareStatement = con.prepareStatement(QUERY);
+        prepareStatement.executeUpdate();
+
         Statement statement = UseJdbc.useStatement();
-        statement.executeUpdate(QUERY);
         ResultSet resultSet = statement.executeQuery("SELECT salary from employee_payroll WHERE name = 'Terissa';");
 
         resultSet.next();
         double salary = resultSet.getDouble(1);
-        UseJdbc.useConnection().close();
+        con.close();
         System.out.println("Connection closed successfully!");
         return salary;
     }
